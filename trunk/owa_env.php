e425a6beba38560cd71999a8d18c1ae01113b13f
padams <padams@e04f549a-e010-0410-95b0-aa3dbaee814a>
2006-05-09 23:44:04 +0000

    moving metrics to plugins dir. y

--- trunk/owa_env.php
+++ trunk/owa_env.php
@@ -7,7 +7,7 @@ define('OWA_INCLUDE_DIR', OWA_BASE_DIR.'/includes/');
 define('OWA_CONF_DIR', OWA_BASE_DIR.'/conf/');
 define('OWA_PEARLOG_DIR', OWA_BASE_DIR.'/includes/Log-1.9.5');
 define('OWA_PLUGINS_DIR', OWA_BASE_DIR.'/plugins/');
-define('OWA_REQ_PLUGINS_DIR', OWA_BASE_DIR.'/event_handlers/');
+define('OWA_REQ_PLUGINS_DIR', OWA_BASE_DIR.'/plugins/event_handlers/');
 define('OWA_METRICS_DIR', dirname(__FILE__).'/metrics/');
 define('OWA_GRAPHS_DIR', dirname(__FILE__).'/graphs/');
 
