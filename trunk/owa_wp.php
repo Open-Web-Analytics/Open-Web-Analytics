fbb1e02a42f94288f0a377a13ebd5a1d780f5015
padams <padams@e04f549a-e010-0410-95b0-aa3dbaee814a>
2006-05-15 06:05:19 +0000

    further refactoring of wordpress plugin and caller class.

--- trunk/owa_wp.php
+++ trunk/owa_wp.php
@@ -17,8 +17,9 @@
 //
 
 require_once('owa_caller.php');
+
 /**
- * Wordpress Plugin class
+ * Wordpress Caller class
  * 
  * @author      Peter Adams <peter@openwebanalytics.com>
  * @copyright   Copyright &copy; 2006 Peter Adams <peter@openwebanalytics.com>
@@ -28,18 +29,6 @@ require_once('owa_caller.php');
  * @version		$Revision$	      
  * @since		owa 1.0.0
  */
-
-/**
-  * Wordpress plugin class
-  * 
-  * @author      Peter Adams <peter@openwebanalytics.com>
-  * @copyright   Copyright &copy; 2006 Peter Adams <peter@openwebanalytics.com>
-  * @license     http://www.gnu.org/copyleft/gpl.html GPL v2.0
-  * @category    owa
-  * @package     owa
-  * @version		$Revision$	      
-  * @since		owa 1.0.0
-  */
 class owa_wp extends owa_caller {
 	
 	/**
@@ -56,10 +45,12 @@ class owa_wp extends owa_caller {
 	function add_link_tracking($link) {
 	
 		if (!empty($_GET[$this->config['feed_subscription_id']])):
-			return $link."&amp;"."from=feed"."&amp;".$this->config['feed_subscription_id']."=".$_GET[$this->config['feed_subscription_id']];
+			return $link."&amp;"."from=feed"."&amp;".$this->config['ns'].$this->config['feed_subscription_id']."=".$_GET[$this->config['feed_subscription_id']];
 		else:
-			return $link;
+			return $link."&amp;"."from=feed";
 		endif;
+		
+		return;
 	
 	}
 	
@@ -105,94 +96,14 @@ class owa_wp extends owa_caller {
 		
 	}
 	
-	function first_request_handler() {
+	function process_comment() {
 		
-		if (isset($_COOKIE[$this->config['ns'].$this->config['first_hit_param']])):
-			$owa = new owa;
-			$owa->process_first_request();
-		endif;
-			
-		header('Content-type: image/gif');
-		header('P3P: CP="NOI NID ADMa OUR IND UNI COM NAV"');
-		header('Expires: Sat, 22 Apr 1978 02:19:00 GMT');
-		header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
-		header('Cache-Control: no-store, no-cache, must-revalidate');
-		header('Cache-Control: post-check=0, pre-check=0', false);
-		header('Pragma: no-cache');
-				
-		printf(
-		  '%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%',
-		  71,73,70,56,57,97,1,0,1,0,128,255,0,192,192,192,0,0,0,33,249,4,1,0,0,0,0,44,0,0,0,0,1,0,1,0,0,2,2,68,1,0,59
-		);
-			
-		return;
-	}
-	
-	function graph_request_handler() {
-		
-		$params = array(
-				'api_call' 		=> $_GET['graph'],
-				'period'			=> $_GET['period']
-			
-			);
-			
 		$owa = new owa;
-		$owa->get_graph($params);
+		$owa->process_comment();
 		return;
+		
 	}
-	
-	function save_config($form_data) {
-		
-		//create the new config array
-		$new_config = array();
-			
-		// needed for following DB queries just in case the various 
-		// implementations of the GUI does not allow you to set this.
-		$new_config['site_id'] = $this->config['site_id'];
-			
-		foreach ($form_data as $key => $value) {
-				
-			if ($key != 'wa_update_options'):
-				// update current config
-				$this->config[$key] = $value;
-				//add to config going to the db
-				$new_config[$key] = $value;
-				
-			endif;
-		}
-		
-		owa_settings::save($new_config);
-		$this->e->info("Configuration changes saved to database.");
-		
-		return;
-	}
-	
-	function reset_config() {
-			
-		$config = $this->config->get_default_config();
-		$this->config->save($config);
-		return;
-				
-	}
-	
-	function load_config_from_db() {
-		
-		$config_from_db = owa_settings::fetch($this->config['site_id']);
-		
-		if (!empty($config_from_db)):
-			
-			foreach ($config_from_db as $key => $value) {
-			
-				$this->config[$key] = $value;
-			
-			}
-					
-		endif;
-		
-		return;
-	}
-		
-	
+
 }
 
 ?>
