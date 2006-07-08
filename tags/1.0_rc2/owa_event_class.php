01f7724dafc4d5ad70bdf48d8ae0600352526b7b
padams <padams@e04f549a-e010-0410-95b0-aa3dbaee814a>
2006-07-08 21:45:26 +0000

    taging 1.0_rc2

---
+++ tags/1.0_rc2/owa_event_class.php
@@ -0,0 +1,142 @@
+<?php
+
+//
+// Open Web Analytics - An Open Source Web Analytics Framework
+//
+// Copyright 2006 Peter Adams. All rights reserved.
+//
+// Licensed under GPL v2.0 http://www.gnu.org/copyleft/gpl.html
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// $Id$
+//
+
+require_once 'owa_settings_class.php';
+require_once 'owa_lib.php';
+require_once 'eventQueue.php';
+
+/**
+ * Abstract OWA Event Class
+ * 
+ * @author      Peter Adams <peter@openwebanalytics.com>
+ * @copyright   Copyright &copy; 2006 Peter Adams <peter@openwebanalytics.com>
+ * @license     http://www.gnu.org/copyleft/gpl.html GPL v2.0
+ * @category    owa
+ * @package     owa
+ * @version		$Revision$	      
+ * @since		owa 1.0.0
+ */
+
+class owa_event {
+	
+	/**
+	 * Configuration
+	 *
+	 * @var array
+	 */
+	var $config;
+	
+	/**
+	 * Event Properties
+	 *
+	 * @var array
+	 */
+	var $properties = array();
+	
+	/**
+	 * Event Queue
+	 *
+	 * @var object
+	 */
+	var $eq;
+	
+	/**
+	 * Error handler
+	 *
+	 * @var object
+	 */
+	var $e;
+	
+	/**
+	 * State
+	 *
+	 * @var string
+	 */
+	var $state;
+	
+	/**
+	 * Constructor
+	 * @access public
+	 */
+	function owa_event() {
+		
+		$this->config = &owa_settings::get_settings();
+		$this->e = &owa_error::get_instance();
+		$this->eq = &eventQueue::get_instance();
+		
+		// Retrieve inbound visitor and session values	
+		$this->properties['inbound_visitor_id'] = $_COOKIE[$this->config['ns'].$this->config['visitor_param']];
+		$this->properties['inbound_session_id'] = $_COOKIE[$this->config['ns'].$this->config['session_param']];
+		
+		// Record time of last request
+		$this->properties['last_req'] = $_COOKIE[$this->config['ns'].$this->config['last_request_param']];
+		
+		//epoc time
+		list($msec, $sec) = explode(" ", microtime());
+		$this->properties['sec'] = $sec;
+		$this->properties['msec'] = $msec;
+		
+		//determine time of request
+		$this->properties['timestamp'] = time();
+		$this->properties['year'] = date("Y", $this->properties['timestamp']);
+		$this->properties['month'] = date("n", $this->properties['timestamp']);
+		$this->properties['day'] = date("d", $this->properties['timestamp']);
+		$this->properties['dayofweek'] = date("D", $this->properties['timestamp']);
+		$this->properties['dayofyear'] = date("z", $this->properties['timestamp']);
+		$this->properties['weekofyear'] = date("W", $this->properties['timestamp']);
+		$this->properties['hour'] = date("G", $this->properties['timestamp']);
+		$this->properties['minute'] = date("i", $this->properties['timestamp']);
+		$this->properties['second'] = date("s", $this->properties['timestamp']);
+		
+		//set default site id. Can be overwriten by caller if needed.
+		$this->properties['site_id'] = $this->config['site_id'];
+		
+		return;
+	}
+	
+	/**
+	 * Logs event to event queue
+	 *
+	 */
+	function log() {
+
+		$this->eq->log($this->properties, $this->state);
+		
+		return;
+	}
+	
+	/**
+	 * Applies calling application specific properties to request
+	 *
+	 * @access 	private
+	 * @param 	array $properties
+	 */
+	function _setProperties($properties = null) {
+	
+		if(!empty($properties)):
+			foreach ($properties as $key => $value) {
+			
+				$this->properties[$key] = $value;
+			}
+		endif;
+		
+		return;	
+	}
+}
+
+?>
---
+++ tags/1.0_rc2/templates/visitors_age.tpl
@@ -0,0 +1,19 @@
+<? if ($visitors_age):?>
+<table width="100%">
+	<TR>
+		<TH>Month of First Visit</TH>
+		<TH>Visitors</TH>
+	</TR>
+	<? foreach ($visitors_age as $row):?>
+	<TR>
+		<TD><?=$this->get_month_label($row['first_session_month']);?> <?=$row['first_session_year'];?></TD>
+		<TD>
+			<a href="<?=$this->make_report_link('visitors_report.php', array('owa_page' => 'visitor_list', 'year2' => $row['first_session_year'], 'month2' => $row['first_session_month'], 'period' => $params['period']));?>">
+			<?=$row['count'];?></a>
+		</TD>
+	</TR>
+	<? endforeach;?>
+</table>
+<?else:?>
+<div>There are no visitors for this time period</div>
+<?endif;?>
