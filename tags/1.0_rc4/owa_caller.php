6cce1b5e25d61180683d6298dd5c703a8eb9ec54
padams <padams@e04f549a-e010-0410-95b0-aa3dbaee814a>
2006-07-16 00:43:11 +0000

    added tag for release 1.0_rc4

---
+++ tags/1.0_rc4/owa_caller.php
@@ -0,0 +1,447 @@
+<?
+
+//
+// Open Web Analytics - An Open Source Web Analytics Framework
+//
+// Copyright 2006 Peter Adams. All rights reserved.
+//
+// Licensed under GPL v2.0 http://www.gnu.org/copyleft/gpl.html
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// $Id$
+//
+
+include_once('owa_env.php');
+require_once 'owa_settings_class.php';
+require_once 'owa_controller.php';
+require_once 'owa_installer.php';
+
+/**
+ * Abstract Caller class used to build application specific invocation classes
+ * 
+ * @author      Peter Adams <peter@openwebanalytics.com>
+ * @copyright   Copyright &copy; 2006 Peter Adams <peter@openwebanalytics.com>
+ * @license     http://www.gnu.org/copyleft/gpl.html GPL v2.0
+ * @category    owa
+ * @package     owa
+ * @version		$Revision$	      
+ * @since		owa 1.0.0
+ */
+class owa_caller {
+	
+	/**
+	 * Configuration
+	 *
+	 * @var array
+	 */
+	var $config;
+	
+	/**
+	 * Error handler
+	 *
+	 * @var object
+	 */
+	var $e;
+	
+	/**
+	 * Instance of Request/Event Controller
+	 *
+	 * @var object
+	 */
+	var $controller;
+	
+	/**
+	 * Constructor
+	 *
+	 * @param array $config
+	 * @return owa_caller
+	 */
+	function owa_caller($config) {
+		
+		$this->config = &owa_settings::get_settings();
+		
+		$this->apply_caller_config($config);
+		
+		if ($this->config['fetch_config_from_db'] == true):
+			$this->load_config_from_db();
+		endif;
+		
+		$this->controller = new owa;
+	
+		return;
+	
+	}
+	
+	/**
+	 * Applies caller specific configuration params on top of 
+	 * those specified on the global OWA config file.
+	 *
+	 * @param array $config
+	 */
+	function apply_caller_config($config) {
+		
+		if (!empty($config)):
+		
+			foreach ($config as $key => $value) {
+				
+				$this->config[$key] = $value;
+				
+			}
+
+		endif;
+					
+		return;
+
+	}
+	
+	/**
+	 * Fetches instance specific configuration params from the database
+	 * 
+	 */
+	function load_config_from_db() {
+		
+		$config_from_db = &owa_settings::fetch($this->config['configuration_id']);
+		
+		if (!empty($config_from_db)):
+			
+			foreach ($config_from_db as $key => $value) {
+			
+				$this->config[$key] = $value;
+			
+			}
+					
+		endif;
+		
+		return;
+	}
+	
+	/**
+	 * Logs a Page Request
+	 *
+	 * @param array $app_params	This is an array of application specific request params
+	 */
+	function log($app_params) {
+		
+		return $this->controller->logEvent('page_request', $app_params);
+		
+	}
+	
+	/**
+	 * Logs any event to the event queue
+	 *
+	 * @param array $app_params
+	 * @param string $event_type
+	 * @return boolean
+	 */
+	function logEvent($event_type, $app_params) {
+		
+		return $this->controller->logEvent($event_type, $app_params);
+		
+	}
+	
+	function install($type) {
+		
+		$this->config['fetch_config_from_db'] = false;
+	    $installer = &owa_installer::get_instance();	   
+	    $install_check = $installer->plugins[$type]->check_for_schema();
+	    
+	    if ($install_check == false):
+		    //Install owa schema
+	    	$status = $installer->plugins[$type]->install(); 
+	    else:
+	    	// owa already installed
+	    	$status = false;
+	    endif;
+	    
+	    return $status;
+		
+	}
+	
+	function reset_config() {
+			
+		$config = $this->config->get_default_config();
+		$this->config->save($config);
+		return;
+				
+	}
+	
+	function options_page() {
+	
+		require_once(OWA_BASE_DIR.'/owa_template.php');
+		require_once(OWA_BASE_DIR.'/owa_news.php');
+		
+		//Fetch latest OWA news
+		$rss = new owa_news;
+		$news = $rss->Get($rss->config['owa_rss_url']);
+
+		//Setup templates
+		$options_page = & new owa_template;
+		$options_page->set_template($options_page->config['report_wrapper']);
+		$options_page->set('news', $news);
+		$body = & new owa_template; 
+		$body->set_template('options.tpl');// This is the inner template
+		$body->set('config', $this->config);
+		$body->set('page_title', 'OWA Options');
+		$options_page->set('content', $body);
+		// Make Page
+		echo $options_page->fetch();
+		
+		return;
+	}
+	
+	function first_request_handler() {
+		
+		header('Content-type: image/gif');
+		header('P3P: CP="'.$this->config['p3p_policy'].'"');
+		header('Expires: Sat, 22 Apr 1978 02:19:00 GMT');
+		header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
+		header('Cache-Control: no-store, no-cache, must-revalidate');
+		header('Cache-Control: post-check=0, pre-check=0', false);
+		header('Pragma: no-cache');
+		
+		$this->e->debug('Handling special first_hit request...');
+		
+		if (!empty($_COOKIE[$this->config['ns'].$this->config['first_hit_param']])):
+			$this->controller->process_first_request();
+		endif;
+			
+		printf(
+		  '%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%',
+		  71,73,70,56,57,97,1,0,1,0,128,255,0,192,192,192,0,0,0,33,249,4,1,0,0,0,0,44,0,0,0,0,1,0,1,0,0,2,2,68,1,0,59
+		);
+			
+		return;
+	}
+	
+	
+	function getGraph($app_params = '') {
+		
+		if(empty($app_params)):
+			$app_params = owa_lib::getRestparams();
+		endif;
+	
+		return $this->controller->getGraph($app_params);
+		
+	}	
+	
+	/**
+	 * Logs event whose properties are specified on the URL
+	 *
+	 * @param unknown_type $event_type
+	 * @return unknown
+	 */
+	function logEventRest($event_type) {
+		
+		$app_params = owa_lib::getRestparams();
+	
+		return $this->controller->logEvent($event_type, $app_params);
+		
+	}
+	
+	/**
+	 * Saves Configuration values to the database
+	 *
+	 * @param array $form_data
+	 */
+	function save_config($form_data) {
+		
+		//create the new config array
+		$new_config = array();
+			
+		// needed for following DB queries just in case the various 
+		// implementations of the GUI does not allow you to set this.
+		$new_config['configuration_id'] = $this->config['configuration_id'];
+			
+		foreach ($form_data as $key => $value) {
+				
+			if ($key != 'wa_update_options'):
+				// update current config
+				$this->config[$key] = $value;
+				//add to config going to the db
+				$new_config[$key] = $value;
+				
+			endif;
+		}
+		
+		owa_settings::save($new_config);
+		$this->e->notice("Configuration changes saved to database.");
+		
+		return;
+	}
+	
+	/**
+	 * Returns All Page Tags
+	 *
+	 * @param boolean $echo
+	 * @return string
+	 */
+	function placePageTags($echo = true) {
+		
+		$tags = $this->firstHitTag($echo);
+		
+		if ($this->config['log_dom_clicks'] == true):
+			$tags .= $this->clickTag($echo);
+		endif;
+		
+		if ($echo === false):
+			return $tags;
+		else:
+			;
+		endif;
+			
+		return;
+		
+	}
+	
+	
+	/**
+	 * Generates First hit javascript tag
+	 *
+	 * @param boolean $echo
+	 * @return string
+	 */
+	function firstHitTag($echo = true) {
+		
+		if (empty($_COOKIE[$this->config['ns'].$this->config['first_hit_param']]) && empty($_COOKIE[$this->config['ns'].$this->config['visitor_param']])):	
+		
+			$bug  = "<script language=\"JavaScript\" type=\"text/javascript\">";
+			$bug .= "document.write('<img src=\"".$this->config['action_url']."?owa_action=".$this->config['first_hit_param']."\">');</script>";
+			//$bug .= "<noscript><img src=\"".$this->config['action_url']."?owa_action=".$this->config['first_hit_param']."\"></noscript>";		
+			if ($echo === false):
+				return $bug;
+			else:
+				echo $bug;
+			endif;
+		endif;
+		
+		return;
+
+	}
+	
+	/**
+	 * Echos the request logger javascript library
+	 *
+	 */
+	function place_log_bug() {
+		
+		$url = $this->config['public_url'].'/page.php?';
+		
+		$bug = 'var owa_url = \'' . $url . '\';';
+		
+		$bug .= file_get_contents(OWA_INCLUDE_DIR.'/webbug.js');
+		
+		echo $bug;
+		
+		return;
+		
+	}
+	
+	function clickTag($echo = true) {
+		
+ 		$tag = sprintf('<SCRIPT TYPE="text/javascript" SRC="%s?owa_action=click_bug&random=%s"></SCRIPT><DIV ID="owa_click_bug"></DIV>', 
+ 						$this->config['action_url'],
+ 						rand());
+ 						
+ 		if ($echo === false):
+			return $tag;
+		else:
+			echo $tag;
+		endif;
+		
+		return;
+
+	}
+	
+	function place_click_bug() {
+		
+		$url = $this->config['action_url'].'?owa_action=log_event&event=click&';
+		
+		$js = file_get_contents(OWA_INCLUDE_DIR.'/clickbug.js');
+		
+		$bug = sprintf($js, $url, $url); 	
+		
+		echo $bug;
+		
+		return;
+		
+	}
+	
+	function requestTag($site_id) {
+		
+		$tag  = '<SCRIPT language="JavaScript">'."\n";
+		$tag .= "\t".sprintf('var owa_site_id = %s', $site_id)."\n";
+		$tag .= '</SCRIPT>'."\n\n";
+ 		$tag .= sprintf('<SCRIPT TYPE="text/javascript" SRC="%s/wb.php"></SCRIPT>', 
+ 						$this->config['public_url']);
+ 		
+ 		return $tag;
+		
+	}
+	
+	
+	/**
+	 * Handler for special action requests
+	 * 
+	 * This is sometimes called on every request by certain frameworks so nothing sounds be outside the 
+	 * switch statement.
+	 *
+	 */
+	function actionRequestHandler() {
+	
+		switch ($_GET['owa_action']) {
+			
+			// This handles requests to log the delayed request contained in first_hit cookie  for new users.
+		    case $this->config['first_hit_param']:
+		    	$this->e->debug('Special action request received: first_hit');
+				$this->first_request_handler();		
+				exit;
+				
+			// This handles requests for graphs	
+			case $this->config['graph_param']:
+				$this->e->debug('Special action request received: graph');
+				$this->getGraph();
+				exit;
+				
+			// This handles requests for the click tracking javascript library	
+			case "click_bug":
+				// This is the handler for javascript request for the logging web bug.
+				$this->e->debug('Special action request received: '.$_GET['owa_action']); 
+				$this->place_click_bug();
+				exit;
+				
+			// This handles requests o log an event via http 	
+			case "log_event":
+				$this->e->debug('Special action request received: '.$_GET['owa_action']);
+				ignore_user_abort(true);
+				$this->logEventRest($_GET['event']);
+				
+				// Return 1x1 pixel
+				header('Content-type: image/gif');
+				header('P3P: CP="'.$l->config['p3p_policy'].'"');
+				header('Expires: Sat, 22 Apr 1978 02:19:00 GMT');
+				header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
+				header('Cache-Control: no-store, no-cache, must-revalidate');
+				header('Cache-Control: post-check=0, pre-check=0', false);
+				header('Pragma: no-cache');
+				
+				printf(
+				  '%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%',
+				  71,73,70,56,57,97,1,0,1,0,128,255,0,192,192,192,0,0,0,33,249,4,1,0,0,0,0,44,0,0,0,0,1,0,1,0,0,2,2,68,1,0,59
+				);
+					
+				exit;
+           		
+        }
+
+        return;
+
+	}
+	
+}
+
+?>
---
+++ tags/1.0_rc4/owa_lib.php
@@ -0,0 +1,450 @@
+<?php
+
+//
+// Open Web Analytics - An Open Source Web Analytics Framework
+//
+// Copyright 2006 Peter Adams. All rights reserved.
+//
+// Licensed under GPL v2.0 http://www.gnu.org/copyleft/gpl.html
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// $Id$
+//
+
+require_once 'owa_env.php';
+require_once (OWA_PEARLOG_DIR . '/Log.php');
+
+/**
+ * Utility Functions
+ * 
+ * @author      Peter Adams <peter@openwebanalytics.com>
+ * @copyright   Copyright &copy; 2006 Peter Adams <peter@openwebanalytics.com>
+ * @license     http://www.gnu.org/copyleft/gpl.html GPL v2.0
+ * @category    owa
+ * @package     owa
+ * @version		$Revision$	      
+ * @since		owa 1.0.0
+ */
+class owa_lib {
+
+	/**
+	 * Convert Associative Array to String
+	 *
+	 * @param string $inner_glue
+	 * @param string $outer_glue
+	 * @param array $array
+	 * @return string 
+	 */
+	function implode_assoc($inner_glue, $outer_glue, $array) {
+	   $output = array();
+	   foreach( $array as $key => $item ) {
+			  $output[] = $key . $inner_glue . $item;
+		}
+		
+		return implode($outer_glue, $output);
+	}
+			
+	/**
+	 * Deconstruct Associative Array 
+	 *
+	 * For example this takes array([1] => array(a => dog, b => cat), [2] => array(a => sheep, b => goat))
+	 * and tunrs it into array([a] => array(dog, sheep), [b] => array(cat, goat)) 
+	 * 
+	 * @param array $a_array
+	 * @return array $data_arrays
+	 * @access public
+	 */
+	function deconstruct_assoc($a_array) {
+		
+		$data_arrays = array();
+	
+		if(!empty($a_array[1])) :
+		
+			foreach ($a_array as $key => $value) {
+				foreach ($value as $k => $v) {
+					$data_arrays[$k][] = $v;
+			
+				}
+			}
+		else:
+			//print_r($a_array[0]);
+			foreach ($a_array[0] as $key => $value) {
+				$data_arrays[$key][] = $value;
+			}
+		endif;
+		
+		return $data_arrays;
+	}
+	
+	
+	function decon_assoc($a_array) {
+		
+		$data_arrays = array();
+	
+		foreach ($a_array as $key => $value) {
+			//foreach ($value as $k => $v) {
+				$data_arrays[$key][] = $value;
+		
+			//}
+		}
+		
+		return $data_arrays;
+	}
+
+	/**
+	 * Array of Current Time
+	 *
+	 * @return array
+	 * @access public
+	 */
+	function time_now() {
+		
+		$timestamp = time();
+		
+		return array(
+			
+				'year' 				=> date("Y", $timestamp),
+				'month' 			=> date("n", $timestamp),
+				'day' 				=> date("d", $timestamp),
+				'dayofweek' 		=> date("w", $timestamp),
+				'dayofyear' 		=> date("z", $timestamp),
+				'weekofyear'		=> date("W", $timestamp),
+				'hour'				=> date("G", $timestamp),
+				'minute' 			=> date("i", $timestamp),
+				'second' 			=> date("s", $timestamp),
+				'timestamp'			=> $timestamp
+			);
+	}
+		
+	/**
+	 * Stub of debug Handler
+	 *
+	 * @return string
+	 * @access 	public
+	 * @static 
+	 */
+	function &get_debugmsgs() {
+		
+		static $msgs;
+		return $msgs;
+	}
+		
+	/**
+	 * Error Handler
+	 *
+	 * @param string $msg
+	 * @access public
+	 */
+	function errorHandler($msg) {
+		
+		$conf = array('mode' => 0755, 'timeFormat' => '%X %x');
+		$error_logger = &Log::singleton('file', $this->config['error_log_file'], 'ident', $conf);
+		$this->error_logger->_lineFormat = '[%3$s]';
+		
+		return;
+	}
+	
+	/**
+	 * Information array for Months in the year.
+	 *
+	 * @return array
+	 */
+	function months() {
+		
+		return array(
+					
+					1 => array('label' => 'January'),
+					2 => array('label' => 'February'),
+					3 => array('label' => 'March'),
+					4 => array('label' => 'April'),
+					5 => array('label' => 'May'),
+					6 => array('label' => 'June'),
+					7 => array('label' => 'July'),
+					8 => array('label' => 'August'),				
+					9 => array('label' => 'September'),
+					10 => array('label' => 'October'),
+					11 => array('label' => 'November'),
+					12 => array('label' => 'December')
+		);
+		
+	}
+	
+	function days() {
+		
+		return array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 
+					15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31);
+	}
+	
+	function years() {
+		
+		static $years;
+		
+		if (empty($years)):
+			
+			$start_year = 2005;
+			
+			$years = array($start_year);
+			
+			$num_years =  date("Y", time()) - $start_year;
+			
+			for($i=1; $i<=$num_years; $i++) {
+		 	
+				$years[] = $start_year + $i;
+			}
+			
+			$years = array_reverse($years);
+		
+		endif;
+		
+		return $years;
+	}
+	
+	
+	/**
+	 * Returns a label from an array of months
+	 *
+	 * @param int $month
+	 * @return string
+	 */
+	function get_month_label($month) {
+		
+		static $months;
+		
+		if (empty($months)):
+
+			$months = owa_lib::months();
+		
+		endif;  
+		
+		return $months[$month]['label'];
+		
+	}
+	
+	
+	/**
+	 * Sets the suffix for Days used in Date labels
+	 *
+	 * @param string $day
+	 * @return string
+	 */
+	function setDaySuffix($day) {
+		
+		switch ($day) {
+			
+			case "1":
+				$day_suffix = 'st';
+				break;
+			case "2":
+				$day_suffix = 'nd';
+				break;
+			case "3":
+				$day_suffix = 'rd';
+				break;
+			default:
+				$day_suffix = 'th';
+		}
+		
+		return $day_suffix;
+		
+	}
+	
+	/**
+	 * Generates the label for a date
+	 *
+	 * @param array $params
+	 * @return string
+	 */
+	function getDatelabel($params) {
+		
+		switch ($params['period']) {
+		
+			case "day":
+				return sprintf("%s, %d%s %s",
+							owa_lib::get_month_label($params['month']),
+							$params['day'],
+							$day_suffix,
+							$params['year']				
+						);
+				break;
+			
+			case "month":
+				return sprintf("%s %s",
+							owa_lib::get_month_label($params['month']),
+							$params['year']				
+						);
+				break;
+			
+			case "year":	
+				return sprintf("%s",
+							$params['year']				
+						);
+				break;
+			case "date_range":
+				return sprintf("%s, %d%s %s - %s, %d%s %s",
+							owa_lib::get_month_label($params['month']),
+							$params['day'],
+							owa_lib::setDaySuffix($params['day']),
+							$params['year'],
+							owa_lib::get_month_label($params['month2']),
+							$params['day2'],
+							owa_lib::setDaySuffix($params['day2']),
+							$params['year2']					
+						);
+				break;
+		}
+		
+		return false;
+		
+	}
+	
+	/**
+	 * Array of Reporting Periods
+	 *
+	 * @return array
+	 */
+	function reporting_periods() {
+		
+		return array(
+					
+					'today' => array('label' => 'Today'),
+					'yesterday' => array('label' => 'Yesterday'),
+					'this_week' => array('label' => 'This Week'),
+					'this_month' => array('label' => 'This Month'),
+					'this_year' => array('label' => 'This Year'),
+					'last_week'  => array('label' => 'Last Week'),
+					'last_month' => array('label' => 'Last Month'),
+					'last_year' => array('label' => 'Last Year'),
+					'last_half_hour' => array('label' => 'The Last 30 Minutes'),				
+					'last_hour' => array('label' => 'Last Hour'),
+					'last_24_hours' => array('label' => 'The Last 24 Hours'),
+					'last_seven_days' => array('label' => 'The Last Seven Days'),
+					'last_thirty_days' => array('label' => 'The Last Thirty Days'),
+					'same_day_last_week' => array('label' => 'Same Day last Week'),
+					'same_week_last_year' => array('label' => 'Same Week Last Year'),
+					'same_month_last_year' => array('label' => 'Same Month Last Year'),
+					//'day' => array('label' => 'Day'),
+					//'month' => array('label' => 'Month'),
+					//'year' => array('label' => 'Year'),
+					//'date_range' => array('label' => 'Date Range')
+		);
+		
+	}
+	
+	/**
+	 * Array of Date specific Reporting Periods
+	 *
+	 * @return array
+	 */
+	function date_reporting_periods() {
+		
+		return array(
+					
+					'day' => array('label' => 'Day'),
+					'month' => array('label' => 'Month'),
+					'year' => array('label' => 'Year'),
+					'date_range' => array('label' => 'Date Range')
+		);
+		
+	}
+	
+	/**
+	 * Gets label for a particular reporting period
+	 *
+	 * @param unknown_type $period
+	 * @return unknown
+	 */
+	function get_period_label($period) {
+	
+		$periods = owa_lib::reporting_periods();
+		
+		return $periods[$period]['label'];
+	}
+	
+	/**
+	 * Assembles the current URL from request params
+	 *
+	 * @return string
+	 */
+	function get_current_url() {
+		
+		$url = 'http';	
+		
+		if($_SERVER['HTTPS']=='on'):
+			$url.= 's';
+		endif;
+		
+		$url .= '://'.$_SERVER['SERVER_NAME'];
+		
+		if($_SERVER['SERVER_PORT'] != 80):
+			$url .= ':'.$_SERVER['SERVER_PORT'];
+		endif;
+		
+		$url .= $_SERVER['REQUEST_URI'];
+		
+		return $url;
+	}
+	
+	/**
+	 * Builds date param array from GET
+	 *
+	 * @return array
+	 */
+	function getRestparams() {
+		
+		$config = &owa_settings::get_settings();
+		
+		$params = array();
+		
+		$params['owa_action'] = $_GET['owa_action'];
+		$params['owa_page'] = $_GET['owa_page'];
+		$params['year'] = $_GET['year'];
+		$params['month'] = $_GET['month'];
+		$params['day'] = $_GET['day'];
+		$params['dayofyear'] = $_GET['dayofyear'];
+		$params['weekofyear'] = $_GET['weekofyear'];
+		$params['hour'] = $_GET['hour'];
+		$params['minute'] = $_GET['minute'];
+		$params['year2'] = $_GET['year2'];
+		$params['month2'] = $_GET['month2'];
+		$params['day2'] = $_GET['day2'];
+		$params['dayofyear2'] = $_GET['dayofyear2'];
+		$params['weekofyear2'] = $_GET['weekofyear2'];
+		$params['hour2'] = $_GET['hour2'];
+		$params['minute2'] = $_GET['minute2'];
+		$params['limit'] = $_GET['limit'];
+		$params['offset'] = $_GET['offset'];
+		$params['sortby'] = $_GET['sortby'];
+		$params['period'] = $_GET['period'];
+		$params['site_id'] = $_GET['site_id'];
+		$params['type'] = $_GET['type'];
+		$params['api_call'] = $_GET['name'];
+		$params['session_id'] = $_GET['session_id'];
+		$params['visitor_id'] = $_GET['visitor_id'];
+		$params['document_id'] = $_GET['document_id'];
+		$params['referer_id'] = $_GET['referer_id'];
+		$params['source'] = $_GET['source'];
+		$params['page_url'] = base64_decode($_GET['page_url']);
+		$params['target_url'] = base64_decode($_GET['target_url']);
+		$params['dom_element_name'] = $_GET['dom_element_name'];
+    	$params['dom_element_value'] = $_GET['dom_element_value'];
+    	$params['dom_element_id'] = $_GET['dom_element_id'];
+    	$params['dom_element_x'] = $_GET['dom_element_x'];
+    	$params['dom_element_y'] = $_GET['dom_element_y'];
+    	$params['dom_element_text'] = $_GET['dom_element_text'];
+    	$params['dom_element_tag'] = $_GET['dom_element_tag'];
+    	$params['click_x'] = $_GET['click_x'];
+    	$params['click_y'] = $_GET['click_y'];
+    	
+		
+	
+		return $params;
+		
+	}
+}
+
+?>
