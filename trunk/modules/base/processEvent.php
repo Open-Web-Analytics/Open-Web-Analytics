8c54d640e019bd4620127dfa834058ea256a0be2
padams <padams@e04f549a-e010-0410-95b0-aa3dbaee814a>
2007-03-03 08:28:18 +0000

    fixing bug with setting request time under php4

--- trunk/modules/base/processEvent.php
+++ trunk/modules/base/processEvent.php
@@ -84,7 +84,7 @@ class owa_processEventController extends owa_controller {
 		$this->event->setIp($this->params['server']['HTTP_X_FORWARDED_FOR'], $this->params['server']['HTTP_CLIENT_IP'], $this->params['server']['REMOTE_ADDR']);
 		
 		// Set all time related properties
-		$this->event->setTime($this->params['server']['REQUEST_TIME']);
+		$this->event->setTime(time());
 		
 		// Set host related properties
 		if ($this->config['resolve_hosts'] = true):
