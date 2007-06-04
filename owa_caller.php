05147596ed5893addf744ad5d27fcd3aafa59bad
padams <peter@oncefuture.com>
2007-06-04 11:17:09 +0000

    adding back in caller db close.

--- owa_caller.php
+++ owa_caller.php
@@ -382,10 +382,10 @@ class owa_caller extends owa_base {
 		endif;
 		
 		//clean up any open db connection
-		//if ($this->config['async_db'] == false):
-		//	$db = &owa_coreAPI::dbSingleton();
-		//	$db->close();
-		//endif;
+		if ($this->config['async_db'] == false):
+			$db = &owa_coreAPI::dbSingleton();
+			$db->close();
+		endif;
 		
 		$init = true;
 		
