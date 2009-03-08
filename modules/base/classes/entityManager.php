491a8f8f4bf4b9b0922464268b01b4b29117ce78
padams <peter@oncefuture.com>
2009-03-08 22:36:05 +0000

    addding feed_requests to non-persistent cache list

--- modules/base/classes/entityManager.php
+++ modules/base/classes/entityManager.php
@@ -46,6 +46,7 @@ class owa_entityManager extends owa_base {
 		$this->cache->setCacheDir(OWA_CACHE_DIR);
 		$this->cache->setNonPersistantCollection('owa_session');
 		$this->cache->setNonPersistantCollection('owa_request');
+		$this->cache->setNonPersistantCollection('owa_feed_request');
 		
 		if (!class_exists('owa_entity')):
 			require_once(OWA_BASE_CLASSES_DIR.'owa_entity.php');	
