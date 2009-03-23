9e50761ae84e6fc7fe6be94e3b1e8b4d506b53ab
padams <padams@e04f549a-e010-0410-95b0-aa3dbaee814a>
2009-03-23 06:56:49 +0000

    moving visitor assignment nad sessionization to requestProcessor as thats the only place we rely on a cookie being set.

--- trunk/modules/base/processRequest.php
+++ trunk/modules/base/processRequest.php
@@ -58,6 +58,10 @@ class owa_processRequestController extends owa_processEventController {
 			return;
 		endif;
 		
+		// assign visitor cookie
+		$this->event->assign_visitor($this->event->get('inbound_visitor_id'));	
+		// sessionize
+		$this->event->sessionize($this->event->get('inbound_session_id'));
 						
 		return;
 		
