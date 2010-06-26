22517d733115544d9ed28f47dde0288a6d3405ec
padams <padams@e04f549a-e010-0410-95b0-aa3dbaee814a>
2010-06-26 02:36:55 +0000

    adding back browser based auth to rest API using a proper controller. closing #13

--- trunk/modules/base/module.php
+++ trunk/modules/base/module.php
@@ -180,11 +180,11 @@ class owa_baseModule extends owa_module {
 		$this->registerCliCommand('build', 'base.build');
 		
 		/// register API methods ///
-		$this->registerApiMethod('getResultSet', array($this, 'getResultSet'), array('metrics', 'dimensions', 'siteId', 'constraints', 'sort', 'limit', 'page', 'offset', 'period', 'startDate', 'endDate', 'startTime', 'endTime', 'format'));
+		$this->registerApiMethod('getResultSet', array($this, 'getResultSet'), array('metrics', 'dimensions', 'siteId', 'constraints', 'sort', 'limit', 'page', 'offset', 'period', 'startDate', 'endDate', 'startTime', 'endTime', 'format'),'','view_reports');
 		
-		$this->registerApiMethod('getDomstreams', array($this, 'getDomstreams'), array( 'startDate', 'endDate', 'document_id', 'siteId', 'resultsPerPage', 'page', 'format'));
+		$this->registerApiMethod('getDomstreams', array($this, 'getDomstreams'), array( 'startDate', 'endDate', 'document_id', 'siteId', 'resultsPerPage', 'page', 'format'),'','view_reports');
 		
-		$this->registerApiMethod('getLatestVisits', array($this, 'getLatestVisits'), array( 'startDate', 'endDate', 'visitorId', 'siteId', 'resultsPerPage', 'page', 'format'));
+		$this->registerApiMethod('getLatestVisits', array($this, 'getLatestVisits'), array( 'startDate', 'endDate', 'visitorId', 'siteId', 'resultsPerPage', 'page', 'format'),'','view_reports');
 		
 		return parent::__construct();
 	}
