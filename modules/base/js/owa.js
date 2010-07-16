c269f50bd1c222b1a009cb40eefcd87b0905ec58
padams <peter@oncefuture.com>
2010-07-16 20:51:49 +0000

    fixing broken referralWebSite detail report by properly encoding constraint param.

--- modules/base/js/owa.js
+++ modules/base/js/owa.js
@@ -228,7 +228,11 @@ OWA.util =  {
 	},
 	
 	urlEncode : function(str) {
-		return encodeURIComponent(str).replace(/!/g, '%21').replace(/'/g, '%27').replace(/\(/g, '%28').replace(/\)/g, '%29').replace(/\*/g, '%2A');
+		 str = (str+'').toString();
+    
+    	// Tilde should be allowed unescaped in future versions of PHP (as reflected below), but if you want to reflect current
+    	// PHP behavior, you would need to add ".replace(/~/g, '%7E');" to the following.
+    	return encodeURIComponent(str).replace(/!/g, '%21').replace(/'/g, '%27').replace(/\(/g, '%28').replace(/\)/g, '%29').replace(/\*/g, '%2A').replace(/%20/g, '+').replace(/~/g, '%7E');
 	}  
 }
 
