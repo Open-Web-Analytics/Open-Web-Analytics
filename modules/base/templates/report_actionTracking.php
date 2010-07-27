133a68fd4c011d7c26464b3576a2b97478cb473d
padams <peter@oncefuture.com>
2010-07-27 05:28:54 +0000

    fixing broken link template substitution.

--- modules/base/templates/report_actionTracking.php
+++ modules/base/templates/report_actionTracking.php
@@ -11,14 +11,14 @@
 				
 				var aurl = '<?php echo $this->makeApiLink(array('do' => 'getResultSet', 
 																  'metrics' => 'actions', 
-																  'dimensions' => 'actionName', 
+																  'dimensions' => 'actionGroup,actionName', 
 																  'sort' => 'actions-', 
 																  'resultsPerPage' => 5,
 																  'format' => 'json'), true);?>';
 																  
 				rsh = new OWA.resultSetExplorer('actionsByNameExplorer');
-				var link = '<?php echo $this->makeLink(array('do' => 'base.reportActionDetail', 'actionName' => '%s'), true);?>';
-				rsh.addLinkToColumn('actionName', link, ['actionName']);
+				var link = '<?php echo $this->makeLink(array('do' => 'base.reportActionDetail', 'actionName' => '%s', 'actionGroup' => '%s'), true);?>';
+				rsh.addLinkToColumn('actionName', link, ['actionName', 'actionGroup']);
 				rsh.asyncQueue.push(['refreshGrid']);
 				rsh.load(aurl, 'grid');
 				</script>
