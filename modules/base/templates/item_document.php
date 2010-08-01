1f029bdf96409509de42ba6f3a473cbfe106e77a
padams <peter@oncefuture.com>
2010-08-01 06:59:29 +0000

    adding document detail panel to page detail report.

--- modules/base/templates/item_document.php
+++ modules/base/templates/item_document.php
@@ -1,7 +1,7 @@
-<div class="owa_document" id="<?php echo $document['id'];?>">
+<div class="owa_document" id="<?php echo $properties->get('id');?>">
 	
-	<div class="title"><?php echo $document['page_title'];?></div>
-	<div class="pagetype"><b>Page Type:</B> <?php echo $document['page_type'];?></div>
-	<div class="url"><?php echo $document['url'];?> (<a href="<?php echo $document['url'];?>">visit page</a>)</div>
+	<div class="title"><?php echo $properties->get('page_title');?></div>
+	<div class="pagetype"><b>Page Type:</B> <?php echo $properties->get('page_type');?></div>
+	<div class="url"><?php echo $properties->get('url');?> (<a href="<?php echo $properties->get('url');?>">visit page</a>)</div>
 	
 </div>
