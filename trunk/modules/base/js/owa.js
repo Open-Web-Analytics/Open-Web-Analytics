2f03dc32e8dbcb5ac9f9569429d0788c2adefef7
padams <padams@e04f549a-e010-0410-95b0-aa3dbaee814a>
2010-11-27 00:25:34 +0000

    removing null character from owa_overlay string

--- trunk/modules/base/js/owa.js
+++ trunk/modules/base/js/owa.js
@@ -662,9 +662,9 @@ OWA.util =  {
 	},
 	
 	utf8_decode : function( str_data ) {
-	    // Converts a UTF-8 encoded string to ISO-8859-1  
+		// Converts a UTF-8 encoded string to ISO-8859-1  
 	    // 
-	    // version: 1008.1718
+	    // version: 1009.2513
 	    // discuss at: http://phpjs.org/functions/utf8_decode
 	    // +   original by: Webtoolkit.info (http://www.webtoolkit.info/)
 	    // +      input by: Aman Gupta
@@ -698,6 +698,57 @@ OWA.util =  {
 	    }
 	 
 	    return tmp_arr.join('');
+	},
+	
+	trim : function (str, charlist) {
+	    // Strips whitespace from the beginning and end of a string  
+	    // 
+	    // version: 1009.2513
+	    // discuss at: http://phpjs.org/functions/trim
+	    // +   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+	    // +   improved by: mdsjack (http://www.mdsjack.bo.it)
+	    // +   improved by: Alexander Ermolaev (http://snippets.dzone.com/user/AlexanderErmolaev)
+	    // +      input by: Erkekjetter
+	    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+	    // +      input by: DxGx
+	    // +   improved by: Steven Levithan (http://blog.stevenlevithan.com)
+	    // +    tweaked by: Jack
+	    // +   bugfixed by: Onno Marsman
+	    // *     example 1: trim('    Kevin van Zonneveld    ');
+	    // *     returns 1: 'Kevin van Zonneveld'
+	    // *     example 2: trim('Hello World', 'Hdle');
+	    // *     returns 2: 'o Wor'
+	    // *     example 3: trim(16, 1);
+	    // *     returns 3: 6
+	    var whitespace, l = 0, i = 0;
+	    str += '';
+	    
+	    if (!charlist) {
+	        // default list
+	        whitespace = " \n\r\t\f\x0b\xa0\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u200b\u2028\u2029\u3000";
+	    } else {
+	        // preg_quote custom list
+	        charlist += '';
+	        whitespace = charlist.replace(/([\[\]\(\)\.\?\/\*\{\}\+\$\^\:])/g, '$1');
+	    }
+	    
+	    l = str.length;
+	    for (i = 0; i < l; i++) {
+	        if (whitespace.indexOf(str.charAt(i)) === -1) {
+	            str = str.substring(i);
+	            break;
+	        }
+	    }
+	    
+	    l = str.length;
+	    for (i = l - 1; i >= 0; i--) {
+	        if (whitespace.indexOf(str.charAt(i)) === -1) {
+	            str = str.substring(0, i + 1);
+	            break;
+	        }
+	    }
+	    
+	    return whitespace.indexOf(str.charAt(0)) === -1 ? str : '';
 	},
 	
 	rand : function(min, max) {
@@ -782,7 +833,7 @@ OWA.util =  {
 	base64_decode: function (data) {
 	    // Decodes string using MIME base64 algorithm  
 	    // 
-	    // version: 1008.1718
+	    // version: 1009.2513
 	    // discuss at: http://phpjs.org/functions/base64_decode
 	    // +   original by: Tyler Akins (http://rumkin.com)
 	    // +   improved by: Thunder.m
