ea564e2f1b2daf6c3e74d53b56f8c99b0f519157
padams <padams@e04f549a-e010-0410-95b0-aa3dbaee814a>
2011-04-29 06:48:25 +0000

    addding patch for firing track.action_logged event. #443

--- trunk/modules/base/handlers/actionHandler.php
+++ trunk/modules/base/handlers/actionHandler.php
@@ -59,6 +59,12 @@ class owa_actionHandler extends owa_observer {
 			$ret = $a->create();
 			
 			if ( $ret ) {
+				// Tell others that "track.action" has been logged 
+				$eq = owa_coreAPI::getEventDispatch(); 
+				$nevent = $eq->makeEvent($event->getEventType().'_logged'); 
+				$nevent->setProperties($event->getProperties()); 
+				$eq->asyncNotify($nevent);
+			
 				return OWA_EHS_EVENT_HANDLED;
 			} else {
 				return OWA_EHS_EVENT_FAILED;
