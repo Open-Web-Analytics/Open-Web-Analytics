5f75fd8139ee3240223e0d675b54483a809ec682
padams <padams@e04f549a-e010-0410-95b0-aa3dbaee814a>
2011-07-02 04:32:55 +0000

    adding check for magic_quotes_gpc INI directive. #500

--- trunk/modules/base/installCheckEnv.php
+++ trunk/modules/base/installCheckEnv.php
@@ -56,6 +56,15 @@ class owa_installCheckEnvController extends owa_installController {
 		
 		// Check permissions on log directory
 		
+		// check for magic_quotes
+		$magic_quotes = ini_get('magic_quotes_gpc');
+		if ( $magic_quotes ) {
+			$errors['magic_quotes_gpc']['name'] = 'magic_quotes_gpc';
+			$errors['magic_quotes_gpc']['value'] = $magic_quotes;
+			$errors['magic_quotes_gpc']['msg'] = "The magic_quotes_gpc PHP INI directive must be set to '0' in order for OWA domstreams to operate correctly.";
+			$bad_environment = true;
+		}
+		
 		// Check for config file and then test the db connection
 		if ($this->c->isConfigFilePresent()) {
 			$config_file_present = true;
