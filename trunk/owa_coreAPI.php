e6531c3aeb3d2c21e0e352538b2d721f4cffdba4
padams <padams@e04f549a-e010-0410-95b0-aa3dbaee814a>
2012-01-13 18:03:24 +0000

    adding optional site_id param to isCurrentUserCapable API method. adding site based permission check to API requests.

--- trunk/owa_coreAPI.php
+++ trunk/owa_coreAPI.php
@@ -239,12 +239,12 @@ class owa_coreAPI {
 	 * check to see if the current user has a capability
 	 * always returns a bool
 	 */
-	public static function isCurrentUserCapable($capability) {
+	public static function isCurrentUserCapable($capability, $site_id = '') {
 		
 		$cu = owa_coreAPI::getCurrentUser();
 		owa_coreAPI::debug("Current User Role: ".$cu->getRole());
 		owa_coreAPI::debug("Current User Authentication: ".$cu->isAuthenticated());
-		$ret = $cu->isCapable($capability);
+		$ret = $cu->isCapable($capability, $site_id);
 		owa_coreAPI::debug("Is current User capable: ".$ret);
 		return $ret;
 	}
