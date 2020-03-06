da82069ba74a2513cd301e0b12823c4da1e443fe
Lauser, Nicolai <n.lauser@winhelp.eu>
2020-03-07 00:16:44 +0100

    Revert "Bugfix: User see site links even if they have no permission"
    This reverts commit c3885de2

--- modules/base/reportGoalFunnel.php
+++ modules/base/reportGoalFunnel.php
@@ -1,151 +1,151 @@
+<?php
 
-<div>
+//
+// Open Web Analytics - An Open Source Web Analytics Framework
+//
+// Copyright 2006 Peter Adams. All rights reserved.
+//
+// Licensed under GPL v2.0 http://www.gnu.org/copyleft/gpl.html
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// $Id$
+//
 
-    Choose a goal: <select id="goalChooser">
-        <?php for ($i = 1; $i <= $numGoals; $i++):?>
-            <option <?php if ($i == $goal_number): echo 'SELECTED'; endif;?> value="<?php $this->out($i, false); ?>">Goal <?php $this->out($i, false); ?></option>
-        <?php endfor; ?>
-    </select>
-</div>
+require_once(OWA_BASE_DIR.'/owa_reportController.php');
 
-<?php if ( $this->get('funnel') ):?>
-    <table class="funnel" border="0" style="min-width:100%;">
-        <tr>
-            <td class="funnelLeft">Prior Page Viewed</td>
-            <td class="funnelMiddle"><h2><?php $this->out($goal_conversion_rate);?> conversion rate</h2></td>
-            <td class="funnelRight" style="text-align:right;">Next Page Viewed</td>
-        </tr>
-        <?php foreach ($funnel as $k => $step):?>
-            <tr>
-                <td width="33%" valign="top" class="funnelLeft" id="entrances_step_<?php $this->out($step['step_number']);?>">
-                    <div class="funnelLargeNumber entranceCount" style="text-align: right;" id="prior_page_count_step_<?php $this->out($step['step_number']);?>">
+/**
+ * Goal Funnel Report Controller
+ * 
+ * @author      Peter Adams <peter@openwebanalytics.com>
+ * @copyright   Copyright &copy; 2006 Peter Adams <peter@openwebanalytics.com>
+ * @license     http://www.gnu.org/copyleft/gpl.html GPL v2.0
+ * @category    owa
+ * @package     owa
+ * @version		$Revision$	      
+ * @since		owa 1.4.0
+ */
 
-                    </div>
-                </td>
-                <td width="33%" valign="top" class="funnelMiddle funnelStep" id="step_<?php $this->out($step['step_number']);?>">
-                    <div class="funnelStepName">Step <?php $this->out($step['step_number']);?>: <?php $this->out($step['name']);?></div>
-                    <div class="funnelStepCount"><?php $this->out($step['visitors']);?> <span class="visitorCountLabel">visitors</span></div>
-                    <div class="funnelStepUrl"><?php $this->out($step['url']);?></div>
-                    <div class="genericHorizontalList" style="padding-top:10px;font-size:12px;">
-                        <ul class="">
+class owa_reportGoalFunnelController extends owa_reportController {
+	
+	function action() {
+		
+		$gm = owa_coreAPI::supportClassFactory('base', 'goalManager', $this->getParam( 'siteId' ) );
+		
+		$goal_number = $this->getParam('goalNumber');
+		
+		if ( ! $goal_number ) {
+			$goal_number = 1;
+		}
+		
+		$goal = $gm->getGoal($goal_number);
+		$funnel = $gm->getGoalFunnel($goal_number);
+		
+		if ( $funnel ) {
+			$goal = $gm->getGoal($goal_number);
+			// find required steps. build a constraint string.
+			$required_step_constraints = '';
+			$steps_count = count($funnel);
+			for ($i=1; $i <= $steps_count ;$i++ ) {
+			
+				if (array_key_exists('is_required', $funnel[$i]) && $funnel[$i]['is_required'] === true) {
+				
+					$required_step_constraints .= 'pagePath=='.$funnel[$i]['url'].',';
+				}
+			}
+			$required_step_constraints = trim($required_step_constraints, ',');
+			
+			//print $required_step_constraints;
+			// get total visits
+			$total_visitors_rs = owa_coreAPI::executeApiCommand(array(
+					'period' 	  => $this->get('period'),
+					'startDate'	  => $this->get('startDate'),
+					'endDate'	  => $this->get('endDate'),
+					'constraints' => $required_step_constraints,
+					'metrics' 	  => 'visitors',
+					'do'		  => 'getResultSet',
+					'siteId'	  => $this->getParam( 'siteId' )	
+			));
+			//print_r($total_visitors_rs);
+			$total_visitors = $total_visitors_rs->getAggregateMetric( 'visitors' );
+			//print "Total visits: $total_visitors";
+			
+			$this->set( 'total_visitors',  $total_visitors);
+			// get visits for each step
+			
+			// add goal url to steps array
+			$funnel[] = array('url' => $goal['details']['goal_url'], 'name' => $goal['goal_name'], 'step_number' => $steps_count + 1);
+			foreach ( $funnel as $k => $step ) {
+				$operator = '==';
+				$rs = owa_coreAPI::executeApiCommand(array(
+						'period' 	  => $this->get('period'),
+						'startDate'	  => $this->get('startDate'),
+						'endDate'	  => $this->get('endDate'),
+						'metrics' 	  => 'visitors',
+						'constraints' => 'pagePath'.$operator.$step['url'],
+						'do'		  => 'getResultSet',
+						'siteId'	  => $this->getParam( 'siteId' )		
+				));
+				
+				$visitors = $rs->getAggregateMetric('visitors') ? $rs->getAggregateMetric('visitors'): 0;
+				$funnel[$k]['visitors'] = $visitors;
+				
+				// backfill check in case there are more visitors to this step than were at prior step.
+				if ($funnel[$k]['visitors'] <= $funnel[$k-1]['visitors']) {
+					if ($funnel[$k-1]['visitors'] > 0 ) {
+						$funnel[$k]['visitor_percentage'] = round($funnel[$k]['visitors'] / $funnel[$k-1]['visitors'], 4) * 100 . '%';
+					} else {
+						$funnel[$k]['visitor_percentage'] = '0.00%';
+					}
+				} else {
+					$funnel[$k]['visitor_percentage'] = '100%';
+				}
+			}
+			
+			//print_r($funnel);
+			
+			$goal_step = end($funnel);
+			$goal_conversion_rate = round($goal_step['visitors'] / $total_visitors, 2) * 100 . '%';
+			$this->set('goal_conversion_rate', $goal_conversion_rate);
+			$this->set('funnel', $funnel);	
+			
+		}			
+		// set view stuff
+		$this->setSubview('base.reportGoalFunnel');
+		$this->setTitle('Funnel Visualization:', 'Goal ' . $goal_number);
+		$this->set('goal_number', $goal_number);
+	}
+}
+		
+require_once(OWA_BASE_DIR.'/owa_view.php');
 
+/**
+ * Goal Funnel Report View
+ * 
+ * @author      Peter Adams <peter@openwebanalytics.com>
+ * @copyright   Copyright &copy; 2006 Peter Adams <peter@openwebanalytics.com>
+ * @license     http://www.gnu.org/copyleft/gpl.html GPL v2.0
+ * @category    owa
+ * @package     owa
+ * @version		$Revision$	      
+ * @since		owa 1.4.0
+ */
 
-                            <li>
-                                <span class="inline_h4"><a href="<?php echo $this->makeLink(array('do' => 'base.reportDomstreams', 'pagePath' => $step['url']), true);?>">Watch Domstreams</a></span>
-                            </li>
+class owa_reportGoalFunnelView extends owa_view {
+	
+	function render() {
+		
+		$this->body->set_template('report_goal_funnel.php');
+		$this->body->set('funnel', $this->get('funnel'));
+		$this->body->set('funnel_json', json_encode($this->get('funnel')));
+		$this->body->set('goal_conversion_rate', $this->get('goal_conversion_rate'));
+		$this->body->set('numGoals', owa_coreAPI::getSetting('base', 'numGoals') );
+		$this->body->set('goal_number',  $this->get('goal_number') );
+	}
+}
 
-                            <li>
-                                <span class="inline_h4"><a href="<?php echo $this->makeLink(array('do' => 'base.reportDomClicks', 'pagePath' => $step['url']), true);?>">Analyze Dom Clicks</a></span>
-                            </li>
-                        </ul>
-                    </div>
-                </td>
-                <td width="33%" valign="top" class="funnelRight" id="exits_step_<?php $this->out($step['step_number']);?>">
-                    <div class="funnelLargeNumber exitCount" id="next_page_count_step_<?php $this->out($step['step_number']);?>"></div>
-                </td>
-            </tr>
-            <?php if (array_key_exists($k+1, $funnel)):?>
-                <tr>
-                    <td class="funnelLeft"></td>
-                    <td class="funnelMiddle funnelLargeNumber funnelFlow">
-                        <?php $this->out($funnel[$k+1]['visitor_percentage']);?><BR>
-                        <span class="secondaryText">Proceeded to step: <?php $this->out($funnel[$k+1]['name']); ?></span>
-                    </td>
-                    <td class="funnelRight"></td>
-                </tr>
-            <?php endif;?>
-        <?php endforeach;?>
-    </table>
-
-    <script>
-        var funnel_json = <?php $this->out($funnel_json, false);?>;
-        var i = 1;
-        for (step in funnel_json) {
-            step = parseInt(step);
-
-            var total_steps = OWA.util.countObjectProperties(funnel_json);
-            var operator = '==';
-            if (i < total_steps ) {
-                next_step = step + 1;
-            } else {
-                next_step = step;
-            }
-
-            if (i == 1) {
-                prior_step = step;
-            } else {
-                prior_step = step - 1 ;
-            }
-
-            // prior pages
-            var name = 'entrances_step_' + funnel_json[step]['step_number'] ;
-            OWA.items[name] = new OWA.resultSetExplorer(name);
-            OWA.items[name].setDataLoadUrl(
-                OWA.items[name].makeApiRequestUrl( 'getResultSet',{
-                    metrics: 'visitors',
-                    dimensions: 'priorPagePath',
-                    sort: 'visitors-',
-                    format: 'json',
-                    constraints: 'pagePath' + operator + funnel_json[step]['url'] + ',priorPagePath!=' + funnel_json[prior_step]['url'],
-                    resultsPerPage: 5,
-                    siteId: OWA.items['base-reportGoalFunnel'].getSiteId(),
-                    period: OWA.items['base-reportGoalFunnel'].getPeriod(),
-                    startDate: OWA.items['base-reportGoalFunnel'].getStartDate(),
-                    endDate: OWA.items['base-reportGoalFunnel'].getEndDate()
-                }));
-            OWA.items[name].asyncQueue.push(['refreshGrid']);
-            OWA.items[name].asyncQueue.push([
-                'renderTemplate',
-                '<*= this.d.resultSet.aggregates.visitors.formatted_value *>',
-                {d: OWA.items[name]},
-                'replace',
-                'prior_page_count_step_' + funnel_json[step]['step_number']
-            ]);
-            OWA.items[name].load();
-            // next page
-            var name = 'exits_step_' + funnel_json[step]['step_number'] ;
-            OWA.items[name] = new OWA.resultSetExplorer(name);
-            OWA.items[name].setDataLoadUrl(
-                OWA.items[name].makeApiRequestUrl( 'getResultSet',{
-                    metrics: 'visitors',
-                    dimensions: 'pagePath',
-                    sort: 'visitors-',
-                    format: 'json',
-                    constraints: 'priorPagePath' + operator + funnel_json[step]['url'] + ',pagePath!=' + funnel_json[next_step]['url'],
-                    resultsPerPage: 5,
-                    siteId: OWA.items['base-reportGoalFunnel'].getSiteId(),
-                    period: OWA.items['base-reportGoalFunnel'].getPeriod(),
-                    startDate: OWA.items['base-reportGoalFunnel'].getStartDate(),
-                    endDate: OWA.items['base-reportGoalFunnel'].getEndDate()
-                }));
-            OWA.items[name].asyncQueue.push([
-                'renderTemplate',
-                '<*= this.d.resultSet.aggregates.visitors.formatted_value *>',
-                {d: OWA.items[name]},
-                'replace',
-                'next_page_count_step_' + funnel_json[step]['step_number']
-            ]);
-            OWA.items[name].asyncQueue.push(['refreshGrid']);
-            OWA.items[name].load();
-            i++;
-        }
-    </script>
-<?php else: ?>
-    No Funnel has been configured for this goal.
-    <?php if ($this->getCurrentUser()->isCapable('edit_settings')): ?>
-        <a href="<?php echo $this->makeLink(array('do' => 'base.optionsGoalEntry', 'goal_number' => $goal_number, 'siteId' => $params['siteId']));?>">Add a funnel</a>
-    <?php endif; ?>
-<?php endif;?>
-
-<script>
-    // jquery binding for select list
-    // Bind event handlers
-    jQuery(document).ready(function(){
-
-        jQuery('#goalChooser').change(function() {
-            var num = jQuery("#goalChooser option:selected").val();
-            OWA.items['base-reportGoalFunnel'].setRequestProperty('goalNumber', num);
-            OWA.items['base-reportGoalFunnel'].reload();
-        });
-    });
-</script>
+?>
