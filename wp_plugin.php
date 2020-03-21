f377615de5e07404cea0190cb44296e89a7d40c0
Peter Adams <peter@oncefuture.com>
2020-03-20 17:49:09 -0700

    setting version for release.

--- wp_plugin.php
+++ wp_plugin.php
@@ -5,7 +5,7 @@ Plugin Name: Open Web Analytics
 Plugin URI: http://www.openwebanalytics.com
 Description: This plugin enables Wordpress blog owners to use the Open Web Analytics Framework.
 Author: Peter Adams
-Version: v1.6.0
+Version: v1.6.5
 Author URI: http://www.openwebanalytics.com
 */
 
