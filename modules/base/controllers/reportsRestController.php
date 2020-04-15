5cee40a6bfc2b0eecad13bdc262fd10472067a10
Peter Adams <peter@oncefuture.com>
2020-04-14 22:45:44 -0700

    added remaining sub reports to /report Rest route.

--- modules/base/controllers/reportsRestController.php
+++ modules/base/controllers/reportsRestController.php
@@ -23,8 +23,7 @@ class owa_reportsRestController extends owa_reportController {
 	
 	function validate() {
 		
-		// if no report name is specified
-		
+		// if no report name is specified do these validations necesary for generic resultSet.
 		if ( ! $this->get('report_name') ) {
 			
 			// metrics are required for resultset queries		
@@ -36,6 +35,37 @@ class owa_reportsRestController extends owa_reportController {
 				$period = owa_coreAPI::supportClassFactory('base', 'timePeriod');	
 				$lables = array_keys($period->getPeriodLabels() );
 				$this->addValidation('period', $this->getParam('period'), 'inArray', array('possible_values' => $lables, 'stopOnError' => true) );				
+			}
+		} else {
+			
+			switch( $this->get('report_name') ) {
+				
+				case 'visit':
+				case 'clickstream':
+				
+					$this->addValidation('sessionId', $this->getParam('sessionId'), 'required', [] );
+					break;
+			
+			
+				case 'latest_visits':
+				case 'latest_actions':
+			
+					$this->addValidation('startDate', $this->getParam('startDate'), 'required', [] );
+					$this->addValidation('endDate', $this->getParam('endDate'), 'required', [] );
+					$this->addValidation('siteId', $this->getParam('siteId'), 'required', [] );
+					break;
+					
+				case 'transaction_items':
+					$this->addValidation('transactionId', $this->getParam('transactionId'), 'required', [] );
+
+					break;
+					
+				case 'transactions':
+				
+					$this->addValidation('siteId', $this->getParam('siteId'), 'required', [] );
+
+					break;
+				
 			}
 		}
 	}
@@ -74,7 +104,7 @@ class owa_reportsRestController extends owa_reportController {
 	
 	function getReport( $report_name ) {
 		
-		$method = 'get_'.$report_name;
+		$method = 'report_'.$report_name;
 		
 		return $this->$method();
 	}
@@ -159,8 +189,324 @@ class owa_reportsRestController extends owa_reportController {
         return  $rsm->getResults();
     }
 
-	
-	
+	function report_visit() {
+		
+        if ( $this->get('sessionId') ) {
+
+            $rs = owa_coreAPI::supportClassFactory('base', 'paginatedResultSet');
+            $db = owa_coreAPI::dbSingleton();
+
+            $s = owa_coreAPI::entityFactory('base.session');
+            $h = owa_coreAPI::entityFactory('base.host');
+            $l = owa_coreAPI::entityFactory('base.location_dim');
+            $ua = owa_coreAPI::entityFactory('base.ua');
+            $d = owa_coreAPI::entityFactory('base.document');
+            $v = owa_coreAPI::entityFactory('base.visitor');
+            $r = owa_coreAPI::entityFactory('base.referer');
+            $sr = owa_coreAPI::entityFactory('base.source_dim');
+            $db->selectFrom($s->getTableName(), 'session');
+
+            $db->join(OWA_SQL_JOIN_LEFT_OUTER, $l->getTableName(), 'location', 'location_id');
+            $db->join(OWA_SQL_JOIN_LEFT_OUTER, $h->getTableName(), 'host', 'host_id');
+            $db->join(OWA_SQL_JOIN_LEFT_OUTER, $ua->getTableName(), 'ua', 'ua_id');
+            $db->join(OWA_SQL_JOIN_LEFT_OUTER, $d->getTableName(), 'document', 'first_page_id');
+            $db->join(OWA_SQL_JOIN_LEFT_OUTER, $v->getTableName(), 'visitor', 'visitor_id');
+            $db->join(OWA_SQL_JOIN_LEFT_OUTER, $r->getTableName(), 'referer', 'referer_id');
+            $db->join(OWA_SQL_JOIN_LEFT_OUTER, $sr->getTableName(), 'source', 'source_id');
+
+            $db->selectColumn('session.timestamp as session_timestamp, session.is_new_visitor as session_is_new_visitor, session.num_pageviews as session_num_pageviews, session.last_req as session_last_req, session.id as session_id, session.user_name as session_user_name, session.site_id as site_id, session.visitor_id as visitor_id, session.medium as medium');
+
+            $db->selectColumn('host.host as host_host');
+            $db->selectColumn('location.city as location_city, location.country as location_country');
+            $db->selectColumn('ua.browser_type as browser_type');
+            $db->selectColumn('document.url as document_url, document.page_title as document_page_title, document.page_type as document_page_type');
+            $db->selectColumn('visitor.user_email as visitor_user_email');
+            $db->selectColumn('source.source_domain as source');
+            $db->selectColumn('referer.url as referer_url, referer.page_title as referer_page_title, referer.snippet as referer_snippet');
+
+            $db->where( 'session.id', $this->get( 'sessionId' ) );
+
+            $results = $rs->generate( $db );
+            $rs->resultsRows = $results;
+
+            return $rs;
+
+        }
+    }
+    
+    function report_latest_visits() {
+
+        $rs = owa_coreAPI::supportClassFactory('base', 'paginatedResultSet');
+        $db = owa_coreAPI::dbSingleton();
+
+        $s = owa_coreAPI::entityFactory('base.session');
+        $h = owa_coreAPI::entityFactory('base.host');
+        $l = owa_coreAPI::entityFactory('base.location_dim');
+        $ua = owa_coreAPI::entityFactory('base.ua');
+        $d = owa_coreAPI::entityFactory('base.document');
+        $v = owa_coreAPI::entityFactory('base.visitor');
+        $r = owa_coreAPI::entityFactory('base.referer');
+        $sr = owa_coreAPI::entityFactory('base.source_dim');
+        $st = owa_coreAPI::entityFactory('base.search_term_dim');
+
+        $db->selectFrom($s->getTableName(), 'session');
+
+        $db->join(OWA_SQL_JOIN_LEFT_OUTER, $l->getTableName(), 'location', 'location_id');
+        $db->join(OWA_SQL_JOIN_LEFT_OUTER, $h->getTableName(), 'host', 'host_id');
+        $db->join(OWA_SQL_JOIN_LEFT_OUTER, $ua->getTableName(), 'ua', 'ua_id');
+        $db->join(OWA_SQL_JOIN_LEFT_OUTER, $d->getTableName(), 'document', 'first_page_id');
+        $db->join(OWA_SQL_JOIN_LEFT_OUTER, $v->getTableName(), 'visitor', 'visitor_id');
+        $db->join(OWA_SQL_JOIN_LEFT_OUTER, $r->getTableName(), 'referer', 'referer_id');
+        $db->join(OWA_SQL_JOIN_LEFT_OUTER, $sr->getTableName(), 'source', 'source_id');
+        $db->join(OWA_SQL_JOIN_LEFT_OUTER, $st->getTableName(), 'search_term', 'referring_search_term_id');
+
+        $db->selectColumn('session.timestamp as session_timestamp, session.is_new_visitor as session_is_new_visitor, session.num_prior_sessions as session_num_prior_visits, session.num_pageviews as session_num_pageviews, session.last_req as session_last_req, session.id as session_id, session.user_name as session_user_name, session.site_id as site_id, session.visitor_id as visitor_id, session.medium as medium');
+
+        $db->selectColumn('host.host as host_host');
+        $db->selectColumn('location.city as location_city, location.country as location_country');
+        $db->selectColumn('ua.browser_type as browser_type');
+        $db->selectColumn('document.url as document_url, document.page_title as document_page_title, document.page_type as document_page_type');
+        $db->selectColumn('visitor.user_email as visitor_user_email');
+        $db->selectColumn('source.source_domain as source');
+        $db->selectColumn('referer.url as referer_url, referer.page_title as referer_page_title, referer.snippet as referer_snippet');
+        $db->selectColumn('search_term.terms as search_term');
+
+        if ( $this->get('visitorId') ) {
+            $db->where('visitor_id', $this->get('visitorId'));
+        }
+
+        if ( $this->get('siteId') ) {
+            $db->where('site_id', $this->get('siteId'));
+        }
+
+        if ( $this->get('startDate') && $this->get('endDate') ) {
+            $db->where('session.yyyymmdd', array('start' => $this->get('startDate'), 'end' => $this->get('endDate') ), 'BETWEEN');
+        }
+
+        $db->orderBy('timestamp', 'DESC');
+
+         // pass limit to rs object if one exists
+        $resultsPerPage = $this->get( 'resultsPerPage' ) ?: 20; 
+        $rs->setLimit( $resultsPerPage );
+		$page = $this->get( 'page' ) ?: 1;
+        // pass page to rs object if one exists
+        $rs->setPage( $page );
+
+        $results = $rs->generate($db);
+        $rs->resultsRows = $results;
+
+        return $rs;
+    }
+
+	function report_latest_actions() {
+
+        $rs = owa_coreAPI::supportClassFactory('base', 'paginatedResultSet');
+        $db = owa_coreAPI::dbSingleton();
+
+        $a = owa_coreAPI::entityFactory('base.action_fact');
+        $d = owa_coreAPI::entityFactory('base.document');
+
+        $db->selectFrom($a->getTableName(), 'action');
+
+        $db->join(OWA_SQL_JOIN_LEFT_OUTER, $d->getTableName(), 'document', 'document_id');
+
+
+        $db->selectColumn('action.timestamp, action.action_name, action.action_label, action.action_group, action.numeric_value');
+        $db->selectColumn('document.url as document_url, document.page_title as document_page_title, document.page_type as document_page_type');
+
+        if ( $this->get( 'visitorId' ) ) {
+            $db->where('action.visitor_id', $this->get( 'visitorId' ) );
+        }
+
+        if ( $this->get( 'sessionId' ) ) {
+            $db->where('action.session_id', $this->get( 'sessionId' ) );
+        }
+
+        if ( $this->get( 'siteId' ) ) {
+            $db->where('site_id', $this->get( 'siteId' ) );
+        }
+
+        if ( $this->get( 'startDate' ) && $this->get( 'endDate' ) ) {
+            $db->where('action.yyyymmdd', array('start' => $this->get( 'startDate' ), 'end' => $this->get( 'endDate' ) ), 'BETWEEN');
+        }
+
+        $db->orderBy('action.timestamp', 'DESC');
+
+        // pass limit to rs object if one exists
+        $resultsPerPage = $this->get( 'resultsPerPage' ) ?: 20; 
+        $rs->setLimit( $resultsPerPage );
+		$page = $this->get( 'page' ) ?: 1;
+        // pass page to rs object if one exists
+        $rs->setPage( $page );
+
+        $results = $rs->generate($db);
+        $rs->resultsRows = $results;
+
+        return $rs;
+
+    }
+    
+    function report_clickstream() {
+
+        $rs = owa_coreAPI::supportClassFactory('base', 'paginatedResultSet');
+        $db = owa_coreAPI::dbSingleton();
+        $db->selectFrom('owa_request', 'request');
+        $db->selectColumn("*");
+        // pass constraints into where clause
+        $db->join(OWA_SQL_JOIN_LEFT_OUTER, 'owa_document', 'document', 'document_id', 'document.id');
+
+        if ($this->get( 'sessionId' ) ) {
+            $db->where('session_id', $this->get( 'sessionId' ) );
+        }
+
+        $db->orderBy('timestamp','DESC');
+
+        // pass limit to rs object if one exists
+        $resultsPerPage = $this->get( 'resultsPerPage' ) ?: 100; 
+        $rs->setLimit( $resultsPerPage );
+		$page = $this->get( 'page' ) ?: 1;
+        // pass page to rs object if one exists
+        $rs->setPage( $page );
+
+        $results = $rs->generate($db);
+        $rs->resultsRows = $results;
+
+        return $rs->formatResults($format);
+    }
+    
+    /**
+     * Retrieves full detail of an ecommerce transaction
+     *
+     * @param    $transactionId    string the id of the transaction you want
+     * @param    $format            string the format you want returned
+     * @return
+     */
+    function report_transaction_items() {
+
+        $t = owa_coreAPI::entityFactory( 'base.commerce_transaction_fact' );
+        $t->getbyColumn('order_id', $this->get( 'transactionId' ) );
+        $trans_detail = array();
+
+        $id = $t->get( 'id' );
+        if ( $id ) {
+            $trans_detail = $t->_getProperties();
+            // fetch line items
+            $db = owa_coreAPI::dbSingleton();
+
+            $db->selectFrom( 'owa_commerce_line_item_fact' );
+            $db->selectColumn( '*' );
+            $db->where( 'order_id', $this->get( 'transactionId' ) );
+            $lis = $db->getAllRows();
+            $trans_detail['line_items'] = $lis;
+        }
+
+        return $trans_detail;
+    }
+
+	function report_transactions() {
+		
+		$sort = $this->get('sort') ?: 'desc';
+		$resultsPerPage = $this->get( 'resultsPerPage' ) ?: 25;
+		$page = $this->get( 'page' ) ?: 1;
+		 
+		
+        $db = owa_coreAPI::dbSingleton();
+        $db->selectFrom('owa_commerce_transaction_fact');
+        $db->selectColumn("*");
+        $db->orderBy('timestamp', $sort);
+        $db->where( 'site_id', $this->get( 'siteId' ) );
+
+        if ( $period ) {
+
+            $p = owa_coreAPI::supportClassFactory('base', 'timePeriod');
+            $p->set( $this->get( 'period' ) );
+            $startDate = $p->startDate->get('yyyymmdd');
+            $endDate = $p->endDate->get('yyyymmdd');
+            
+        } else {
+	        
+	        $startDate = $this->get( 'startDate' );
+            $endDate = $this->get( 'endDate' );
+        }
+
+        if ( $startDate && $endDate ) {
+            $db->where('yyyymmdd', array('start' => $startDate, 'end' => $endDate), 'BETWEEN');
+        }
+
+        $rs = owa_coreAPI::supportClassFactory('base', 'paginatedResultSet');
+
+        // pass limit to rs object if one exists
+        $rs->setLimit($resultsPerPage);
+
+        // pass page to rs object if one exists
+        $rs->setPage($page);
+
+        $results = $rs->generate($db);
+
+        return $rs;
+    }
+
+function report_clicks() {
+		
+		$resultsPerPage = $this->get( 'resultsPerPage' ) ?: 100;
+		$page = $this->get( 'page' ) ?: 1;
+		
+        // Fetch document object
+        $d = owa_coreAPI::entityFactory('base.document');
+
+        if ( ! $this->get('document_id') ) {
+
+            $eq = owa_coreAPI::getEventDispatch();
+            $document_id = $d->generateId( $eq->filter('page_url',  urldecode( $this->get('pageUrl') ), $this->get('siteId') ) ) ;
+            
+        } else {
+	        
+	        $document_id = $this->get('document_id');
+        }
+
+        $d->getByColumn('id', $document_id);
+
+
+        $rs = owa_coreAPI::supportClassFactory('base', 'paginatedResultSet');
+        $db = owa_coreAPI::dbSingleton();
+        $db->selectFrom('owa_click');
+        $db->selectColumn("click_x as x,
+                            click_y as y,
+                            page_width,
+                            page_height,
+                            dom_element_x,
+                            dom_element_y,
+                            position");
+
+
+        $db->orderBy('click_y', 'ASC');
+        $db->where('document_id', $document_id);
+        $db->where('site_id', $this->get('siteId'));
+
+
+        if ( $this->get('period') ) {
+
+            $p = owa_coreAPI::supportClassFactory('base', 'timePeriod');
+            $p->set($this->get('period'));
+            $startDate = $p->startDate->get('yyyymmdd');
+            $endDate = $p->endDate->get('yyyymmdd');
+        }
+
+        if ( $this->get('startDate') && $this->get('endDate') ) {
+            $db->where('yyyymmdd', array( 'start' => $this->get('startDate'), 'end' => $this->get('endDate') ), 'BETWEEN');
+        }
+
+        // pass limit to rs object if one exists
+        $rs->setLimit($resultsPerPage);
+
+        // pass page to rs object if one exists
+        $rs->setPage($page);
+
+        $results = $rs->generate($db);
+
+        return $rs;
+    }
+
 	
 }	
 
