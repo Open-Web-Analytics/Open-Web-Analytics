57294484dcb5192b9b7bd9f9a4121f4a0b3b7810
padams <peter@openwebanalytics.com>
2020-05-01 17:22:23 +0000

    fixing double parens.

--- modules/base/templates/documentNavSum.php
+++ modules/base/templates/documentNavSum.php
@@ -1,7 +1,19 @@
 <div style="background-image: url('<?php echo $this->makeImageLink('base/i/document_icon.gif');?>');background-repeat: no-repeat; padding:5px 5px 5px 35px; background-position:13px 5px;">
     <span class="inline_h4">
         <a href="<?php echo $row['document_url'];?>"><?php echo $row['document_page_title'];?></a> &nbsp;
-        (<?php if ( $row['document_page_type'] ): echo $row['document_page_type']; endif;?>)
+        <?php 
+	        
+	        if ( $this->isValueSet( $row['document_page_type'] ) ) {
+	        
+	        	echo '('. $row['document_page_type'] .')';
+	        	 
+	        
+	        } else {
+		        
+		        echo $row['document_page_type'];
+	        }
+	        
+	        ?>
 
         <BR>
         <span class="externalUrl">
