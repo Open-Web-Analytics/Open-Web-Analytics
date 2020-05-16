afd6f0bc166d494f4809f8172d9556efd2c91b81
Peter Adams <peter@oncefuture.com>
2020-05-16 14:23:49 -0700

    removing dependancy on php tracker for invoking owa core.

--- log.php
+++ log.php
@@ -79,13 +79,13 @@ flush();
 ob_end_flush();
 
 // Create instance of OWA
-require_once(OWA_BASE_DIR.'/owa_php.php');
+require_once(OWA_BASE_DIR.'/owa.php');
 $config = array(
 
     'tracking_mode' => true
 );
 
-$owa = new owa_php( $config );
+$owa = new owa( $config );
 
 // check to see if this endpoint is enabled.
 if ( $owa->isEndpointEnabled( basename( __FILE__ ) ) ) {
