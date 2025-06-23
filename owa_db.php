7831393883aa55243d18a8cee444adff1b6bfe61
criterion9 <andrew.j.wallace4@gmail.com>
2025-06-22 22:57:33 -0400

    Criterion9 php 8 db handling (#904)
    * Update owa_db_mysql.php - try-catch around mysqli-query
    
    * Update owa_db.php - handle query with no results
    
    added a check to handle when a query has no results instead of referencing an index that doesn't exist, and return null instead of generating an error.
    
    * Update owa_db_mysql.php
    
    Added logging of caught exceptions

--- owa_db.php
+++ owa_db.php
@@ -353,7 +353,7 @@ class owa_db extends owa_base {
 
         $this->limit(1);
         $ret = $this->_selectQuery();
-        return $ret[0];
+        return is_null($ret)?null:$ret[0];
     }
 
     function _setSql($sql) {
